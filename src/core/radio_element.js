4cd6e42a7485bbf9ca64b67fe7cd6096e9212b64
christian <christian@1ed5ca13-5fb8-644a-9ef7-ad08ef2e8add>
2008-07-30 21:58:25 +0000

    Worked on the FieldElement implementation
    git-svn-id: https://svn.cjohansen.no/validatious2/trunk@2322 1ed5ca13-5fb8-644a-9ef7-ad08ef2e8add

---
+++ src/core/radio_element.js
@@ -0,0 +1,78 @@
+/**
+ * Radio button fields with validation logic
+ *
+ * @implements v2.Composite
+ * @implements v2.FormItem
+ * @implements v2.FieldElement
+ */
+v2.RadioElement = v2.Field.extend(/** @scope v2.RadioElement.prototype */{
+  /**
+   * Construct a new radio field
+   * @see v2.Field.constructor
+   */
+  constructor: function(elements, instant, instantWhenValidated) {
+    // Regular initialization, override events
+    this.base(elements, instant, instantWhenValidated, ['click', 'change']);
+  },
+
+  /**
+   * @see v2.FieldElement.getValue
+   *
+   * Returns the value attribute of the currently selected radio button
+   */
+  getValue: function() {
+    for (var i = 0, radio; (radio = this.__elements[i]); i++) {
+      if (radio.selected) {
+        return radio.value;
+      }
+    }
+
+    return null;
+  },
+
+  /**
+   * @see v2.FieldElement.getLabel
+   *
+   * A radio button field is assumed to be either a list - ordered or unordered -
+   * with some element in front that acts as a label. This may be any element.
+   *
+   * If it is not in a list (ie the element does not have <li> parent elements),
+   * the label is assumed to be the element before the first input element.
+   *
+   * Example (list approach):
+   *
+   *   <h2>Favourite food:</h2>
+   *   <ul>
+   *     <li>
+   *       <input type="radio" name="food" value="hamburger" id="food_hamburger" />
+   *       <label for="food_hamburger">Haburger</label>
+   *     </li>
+   *     <li>
+   *       <input type="radio" name="food" value="pizza" id="food_pizza" />
+   *       <label for="food_pizza">Pizza</label>
+   *     </li>
+   *   </ul>
+   *
+   * getLabel() will in this case return the h2 element.
+   *
+   * Example (no list).
+   *
+   *   <label class="groupLabel">Favourite food:</label>
+   *   <input type="radio" name="food" value="hamburger" id="food_hamburger" />
+   *   <label for="food_hamburger">Haburger</label>
+   *   <input type="radio" name="food" value="pizza" id="food_pizza" />
+   *   <label for="food_pizza">Pizza</label>
+   *
+   * getLabel() will in this case return the first label element
+   */
+  getLabel: function() {
+
+  },
+
+  /**
+   * @see v2.FieldElement.getParent
+   */
+  getParent: function() {
+
+  }
+});
