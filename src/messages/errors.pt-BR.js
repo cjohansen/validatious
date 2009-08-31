fdba092c851fcd1f52cc704c4f7b4be26ca51a51
Lucas Pirola <lucaspirola@gmail.com>
2009-08-30 23:20:00 -0300

    added portuguese translation file;

---
+++ src/messages/errors.pt-BR.js
@@ -0,0 +1,20 @@
+/**
+ * Brazilian Portuguse default error messages
+ */
+v2.$msg({
+  'alpha': '${field} deve conter apenas letras',
+  'alphanum': '${field} deve conter letras e números',
+  'car-regnum-nor': '${field} deve ser um número válido de carteira motorista',
+  'confirmation-of': '${field} deve ser igual ao campo ${field-id}',
+  'email': '${field} deve ser um endereço de email válido',
+  'max-length': '${field} não deve conter mais que ${max} caracteres',
+  'max-val': '${field} não deve ser maior que ${min}',
+  'min-length': '${field} deve conter ao menos ${min} caracteres',
+  'min-val': '${field} deve ser no mínimo ${min}',
+  'numeric': '${field} deve conter apenas números',
+  'phone-nor': '${field} deve ser um número de telefone válido',
+  'required': '${field} é obrigatório',
+  'ssn-nor': '${field} deve ser um número de seguro social válido',
+  'url': '${field} deve ser uma URL válida',
+  'word': '${field} deve conter letras, números ou pontuação'
+});
