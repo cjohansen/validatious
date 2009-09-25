96775bc0badef89ef56c971bf79925b69a0af372
Jonas Grimfelt <grimen@gmail.com>
2009-09-25 21:10:48 +0200

    Added Swedish translation of error messages: errors.sv.js

---
+++ src/messages/errors.sv.js
@@ -0,0 +1,22 @@
+/**
+ * Swedish default error messages
+ *
+ * @depends ../core/message.js
+ */
+v2.$msg({
+  'alpha': '${field} får enbart innehålla bokstäver',
+  'alphanum': '${field} får enbart innehålla bokstäver och/eller siffror',
+  'confirmation-of': '${field} måsta vara samma som ${field-id}',
+  'email': '${field} måste vara en giltig e-postaddress',
+  'max-length': '${field} får inte vara längre än ${max} karaktärer'
+  'max-val': '${field} får inte vara större än ${min}',
+  'min-length': '${field} måste vara minst ${min} karaktärer lång',
+  'min-val': '${field} får inte vara mindre än ${min}',
+  'numeric': '${field} får enbart bestå av siffror',
+  'required': '${field} är obligatorisk',
+  'url': '${field} måste vara en giltig URL',
+  'word': '${field} får enbart innehålla bokstäver, siffor och/eller interpunktion',
+  'phone-se': '${field} måste vara ett giltigt telefonnummer',
+  'ssn-se': '${field} måste vara ett giltigt personnummer',
+  'orgn-se': '${field} måste vara ett giltigt organisationsnummer'
+});
