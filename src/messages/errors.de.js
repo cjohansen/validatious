2f4522ee3c9826fda05ded5b19b7e38bfd3a4312
Christian Johansen <christian@shortcut.no>
2009-11-17 08:55:57 +0100

    Add german error messages, courtesy of Matthias Zeis

---
+++ src/messages/errors.de.js
@@ -0,0 +1,20 @@
+/**
+ * German default error messages
+ */
+v2.$msg({
+  'alpha': '${field} sollte nur Buchstaben enthalten',
+  'alphanum': '${field} sollte nur Buchstaben und Ziffern enthalten',
+  'car-regnum-nor': '${field} sollte ein g&uuml;ltiges norwegisches Kfz-Kennzeichen sein',
+  'confirmation-of': '${field} sollte eine Best&auml;tigung von ${field-id} sein',
+  'email': '${field} sollte eine g&uuml;ltige E-Mail-Adresse sein',
+  'max-length': '${field} sollte nicht mehr als ${max} Zeichen lang sein',
+  'max-val': '${field} sollte nicht gr&ouml;&szlig;er als ${max} sein',
+  'min-length': '${field} sollte zumindest ${min} Zeichen lang sein',
+  'min-val': '${field} sollte zumindest ${min} sein',
+  'numeric': '${field} sollte nur Zahlen enthalten',
+  'phone-nor': '${field} sollte eine g&uuml;ltige norwegische Telefonnummer sein',
+  'required': '${field} ist erforderlich',
+  'ssn-nor': '${field} sollte eine g&uuml;ltige norwegische Sozialversicherungsnummer sein',
+  'url': '${field} sollte eine g&uuml;ltige URL sein',
+  'word': '${field} sollte nur Buchstaben, Ziffern und Satzzeichen enthalten'
+});
